58e820a3f805fb89c71365cccae0263a0d95c2e5
Michel <michel123geb@gmail.com>
2026-07-06 18:46:36 +0300

    Bump SW cache version + clients.claim() to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bulkmeter-v1';
+const CACHE = 'bulkmeter-v2';
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -16,7 +16,7 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
 
